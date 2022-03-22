e9c460539cde1acadcc1887877643da4deee4490
Aniket Prakash <b218004@iiit-bh.ac.in>
2022-03-22 23:02:13 +0530

    feat: setup project & basic routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import {BrowserRouter} from "react-router-dom";
 import { makeServer } from "./server";
 
 // Call make Server
@@ -9,7 +9,9 @@ makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>  
   </React.StrictMode>,
   document.getElementById("root")
 );
